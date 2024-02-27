936a029aa95f3a8980daf94d7fef96bc32d1cd71
Vikas Mishra <70229856+vikasedu-github@users.noreply.github.com>
2024-02-27 13:00:57 +0530

    Update ProductService.cs

--- sqlapp/Services/ProductService.cs
+++ sqlapp/Services/ProductService.cs
@@ -10,7 +10,7 @@ namespace sqlapp.Services
         private static string db_source = "tempdbserver.database.windows.net";
         private static string db_user = "vikas";
         private static string db_password = "admin@123";
-        private static string db_database = "testdb";
+        private static string db_database = "tempsqldb";
 
         private SqlConnection GetConnection()
         {
